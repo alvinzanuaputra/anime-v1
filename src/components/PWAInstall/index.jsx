c798f11ac16424ffdcb07e1b599f9e0c4bd346e0
alvinzanuaputra <5025231064@student.its.ac.id>
2025-09-13 17:52:30 +0700

    add: ducanh next pwa setup

--- src/components/PWAInstall/index.jsx
+++ src/components/PWAInstall/index.jsx
@@ -49,7 +49,7 @@ const PWAInstallButton = () => {
 
   if (isInstalled) {
     return (
-      <div className="flex items-center bg-color-dark text-color-primary rounded-lg border border-color-accent py-1 px-8 lg:font-bold hover:text-color-accent hover:bg-color-dark hover:border-color-dark duration-500 transition-all text-[14px] lg:text-[16px] lg:py-1 sm:text-[16px] sm:py-1 gap-x-1 cursor-not-allowed">
+      <div className="flex items-center bg-color-dark text-color-primary rounded-lg border border-color-accent py-1 px-6 lg:font-bold hover:text-color-accent hover:bg-color-dark hover:border-color-dark duration-500 transition-all text-[14px] lg:text-[16px] lg:py-1 sm:text-[16px] sm:py-1 gap-x-1 cursor-not-allowed">
         <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
           <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
         </svg>
